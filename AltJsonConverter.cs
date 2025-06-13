773651ad6270130563a03b7aff597a3d67534506
Andrew Bradley <andrew.charles.bradley@gmail.com>
2025-06-13 11:20:57 -0400

    nullable

--- AltJsonConverter.cs
+++ AltJsonConverter.cs
@@ -4,15 +4,15 @@ using System.Text.Json.Serialization;
 
 namespace SpotAR.API;
 
-public class AltJsonConverter : JsonConverter<float>
+public class AltJsonConverter : JsonConverter<float?>
 {
-    public override float Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override float? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (reader.TryGetSingle(out float value)) return value; else return 0f;
     }
 
-    public override void Write(Utf8JsonWriter writer, float value, JsonSerializerOptions options)
+    public override void Write(Utf8JsonWriter writer, float? value, JsonSerializerOptions options)
     {
-        writer.WriteNumberValue(value);
+        writer.WriteNumberValue(value ?? 0f);
     }
 }
